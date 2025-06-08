7bea9ec6870b61d4eca4a78b5a2f3f194098147a
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-06-08 16:21:57 +0600

    added new file

---
+++ src/components/Header/Hero.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+
+const Hero = () => {
+    const navigate = useNavigate();
+
+    return (
+        <section className="bg-gradient-to-br from-[#9f70fd] to-[#a167ff] my-10">
+            <div className="max-w-6xl mx-auto text-center pt-5 px-4">
+                <h1 className="text-4xl sm:text-5xl font-extrabold text-white mb-4">
+                    Upgrade Your Tech Accessorize with <br /> Gadget Heaven Accessories
+                </h1>
+                <p className="text-gray-100 mb-6 max-w-xl mx-auto">
+                    Explore the latest gadgets that will take your experience to the next level.
+                    From smart devices to the coolest accessories, we have it all!
+                </p>
+
+                <div className='text-center'>
+                    <button
+                        onClick={() => navigate('/dashboard')}
+                        className="bg-white btn btn-ghost text-purple-600 font-semibold px-6 py-2 rounded-full">
+                        Shop Now
+                    </button>
+                </div>
+                {/* Image Card */}
+                <div className="mt-10 p-1 bg-white rounded-3xl shadow-lg inline-block">
+                    <img
+                        src="/src/assets/banner.jpg" // 👈 Use your actual image path
+                        alt="Gadget Display"
+                        className="rounded-2xl w-full max-w-5xl object-cover"
+                    />
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default Hero;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import Root from './components/MainLayout/Root';
+import Hero from './components/Header/Hero';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Hero></Hero>
+      }
+    ]
   },
 ]);
 createRoot(document.getElementById('root')).render(
